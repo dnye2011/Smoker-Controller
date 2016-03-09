100bf6d82449ca0b563a3b005c91bee4cb62bc69
alanlgirard <algirard8@gmail.com>
2016-03-09 18:35:29 -0500

    Create app.css

--- index.php
+++ index.php
@@ -2,141 +2,127 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-  <script type="text/javascript" src="//code.jquery.com/jquery-git.js"></script>  
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <script type="text/javascript" src="//code.jquery.com/jquery-git.js"></script>
+    <script src="http://code.highcharts.com/highcharts.js"></script>
+    <link rel="stylesheet" href="src/app.css">
+    <title>Smoker Controller</title>
+    <script type='text/javascript'>
+        function updateGraph(){
+        	$(function(){
+        		var pitTemps = [];
+        		var foodTemps = [];
+        		var x_values = [];
+        		var temp = 0;
+        		var timeStr;
 
-  <title>Highcharts test tool by highcharts</title>
-<script type='text/javascript'>
-function updateGraph(){
-	$(function(){
-		var pitTemps = [];
-		var foodTemps = [];
-		var x_values = [];
-		var temp = 0;
-		var timeStr;
-		
-		$.get('gpio.php', { mode:"graph" }, function(data) {
-				data = data.split('/');
-				var tempSwitch = false;
-				for (var i in data)
-				{
-					if (tempSwitch == false)
-					{
-						pitTemps.push(parseInt(data[i]));
-						tempSwitch = true;
-					}
-					else
-					{
-						foodTemps.push(parseInt(data[i]));
-						tempSwitch = false;
-					}
-					timeStr = Math.floor(temp/3600) + 'h ' + Math.floor((temp % 3600)/60) 
-							+ 'm ' + (temp % 60) + "s";
-					x_values.push(timeStr);
-					temp = temp + 5;
-				}
-				pitTemps.pop();
-				
-				$('#container').highcharts({
-					chart : {
-						type : 'line',
-						zoomType: 'x'
-					},
-					title : {
-						text : 'Smoker Temperature'
-					},
-					xAxis : {
-						title : {
-							text : 'Time'
-						},
-						categories : x_values
-					},
-					yAxis : {
-						title : {
-							text: 'Temperature'
-						}
-					},
-					series: [{
-						name : 'Brisket Temperature',
-						data : foodTemps
-					},{
-						name : 'Pit Temp',
-						data : pitTemps
-					}],
-				});
-			}
-		)
-	});
-}
+        		$.get('gpio.php', { mode:"graph" }, function(data) {
+        				data = data.split('/');
+        				var tempSwitch = false;
+        				for (var i in data)
+        				{
+        					if (tempSwitch == false)
+        					{
+        						pitTemps.push(parseInt(data[i]));
+        						tempSwitch = true;
+        					}
+        					else
+        					{
+        						foodTemps.push(parseInt(data[i]));
+        						tempSwitch = false;
+        					}
+        					timeStr = Math.floor(temp/3600) + 'h ' + Math.floor((temp % 3600)/60)
+        							+ 'm ' + (temp % 60) + "s";
+        					x_values.push(timeStr);
+        					temp = temp + 5;
+        				}
+        				pitTemps.pop();
 
-function updateTableData(){
-	var request = new XMLHttpRequest();
-	request.open( "Get" , "gpio.php", true);
-	request.send(null);
-	request.onreadystatechange = function () {
-		if(request.readyState == 4 && request.status == 200){
-			data = request.responseText;
-			var splitData = data.split(",");
-			document.getElementById("pitTemp").innerHTML = splitData[0];
-			document.getElementById("foodTemp").innerHTML = splitData[1];
-			document.getElementById("setTemp").innerHTML = splitData[2];
-			if(splitData[3] == 1){
-				document.getElementById("fan").innerHTML = "On";}
-			else{
-				document.getElementById("fan").innerHTML = "Off";}
-			document.getElementById("fanCount").innerHTML = convertTime(splitData[4]);
-		}
-	}
-}
+        				$('#container').highcharts({
+        					chart : {
+        						type : 'line',
+        						zoomType: 'x'
+        					},
+        					title : {
+        						text : 'Smoker Temperature'
+        					},
+        					xAxis : {
+        						title : {
+        							text : 'Time'
+        						},
+        						categories : x_values
+        					},
+        					yAxis : {
+        						title : {
+        							text: 'Temperature'
+        						}
+        					},
+        					series: [{
+        						name : 'Brisket Temperature',
+        						data : foodTemps
+        					},{
+        						name : 'Pit Temp',
+        						data : pitTemps
+        					}],
+        				});
+        			}
+        		)
+        	});
+        }
 
-function convertTime(count){
-	var seconds = count * 5;
-	var minutes = Math.floor(seconds/60);
-	seconds = seconds % 60;
-	var result = minutes + 'm ' + seconds + 's';
-	return result;
-}
-	
+        function updateTableData(){
+        	var request = new XMLHttpRequest();
+        	request.open( "Get" , "gpio.php", true);
+        	request.send(null);
+        	request.onreadystatechange = function () {
+        		if(request.readyState == 4 && request.status == 200){
+        			data = request.responseText;
+        			var splitData = data.split(",");
+        			document.getElementById("pitTemp").innerHTML = splitData[0];
+        			document.getElementById("foodTemp").innerHTML = splitData[1];
+        			document.getElementById("setTemp").innerHTML = splitData[2];
+        			if(splitData[3] == 1){
+        				document.getElementById("fan").innerHTML = "On";}
+        			else{
+        				document.getElementById("fan").innerHTML = "Off";}
+        			document.getElementById("fanCount").innerHTML = convertTime(splitData[4]);
+        		}
+        	}
+        }
 
-updateTableData();
-updateGraph();
-setInterval(function(){
-	updateTableData();
-}, 5000);
+        function convertTime(count){
+        	var seconds = count * 5;
+        	var minutes = Math.floor(seconds/60);
+        	seconds = seconds % 60;
+        	var result = minutes + 'm ' + seconds + 's';
+        	return result;
+        }
 
-setInterval(function(){
-	updateGraph();
-}, 60000);
+        updateTableData();
+        updateGraph();
+        setInterval(function(){
+        	updateTableData();
+        }, 5000);
 
-function updateSetPoint(){
-	var tempSetPoint = document.getElementById('newSetTemp').value;
-	$.get('setTemp.php', { setTemp: tempSetPoint });
-	alert("New fan set point: " + tempSetPoint);
-	document.getElementById('newSetTemp').value = "";
-	document.getElementById('setTemp').innerHTML = tempSetPoint;
-}
+        setInterval(function(){
+        	updateGraph();
+        }, 60000);
 
-</script>
-<style>
-h1 {
-    font-family: "Arial";
-    font-size: 30px;
-}
-
-td {
-    font-family: "Arial";
-    font-size: 35px;
-	text-align: center;
-}
-
-table {
-    border-collapse: collapse;
-}
-</style>
+        function updateSetPoint(){
+        	var tempSetPoint = document.getElementById('newSetTemp').value;
+        	$.get('setTemp.php', { setTemp: tempSetPoint });
+        	alert("New fan set point: " + tempSetPoint);
+        	document.getElementById('newSetTemp').value = "";
+        	document.getElementById('setTemp').innerHTML = tempSetPoint;
+        }
+    </script>
 </head>
+
 <body>
-  <script src="http://code.highcharts.com/highcharts.js"></script>
-	<H1 id="p1">Smoker Data Output</H1>
+    <div class="toolbar">
+        <H1 id="p1">Smoker Data Output</H1>
+    </div>
+
 	<table border="1">
 		<tr>
 			<td>Pit Temp</td><td>Brisket Temp</td><td>Fan Set Temp</td><td>Fan Status</td><td>Fan Count</td>
@@ -146,9 +132,9 @@ table {
 			<td id="foodTemp"></td>
 			<td id="setTemp"></td>
 			<td id="fan"></td>
-			<td id="fanCount"></td>			
+			<td id="fanCount"></td>
 		</tr>
-		
+
 	</table>
 	<br>
 	<br>
@@ -159,4 +145,3 @@ table {
 	<div id="container" style="height: 500px"></div>
 </body>
 </html>
-
